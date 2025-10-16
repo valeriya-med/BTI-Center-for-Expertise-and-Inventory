4bd63be17a43e0e52481c96b03446a23c0110be9
Valeriya M <valeriyamedwedewa2005@gmail.com>
2025-10-16 13:16:44 +0000

    Fix WSGI, migrations and static files

--- BTIWebsite/wsgi.py
+++ BTIWebsite/wsgi.py
@@ -1,12 +1,19 @@
 import os
 import sys
 
-# шлях до проєкту
-path = '/home/ValeriyaM/BTI-Center-for-Expertise-and-Inventory'
-if path not in sys.path:
-    sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'BTIWebsite.settings'
+project_home = '/home/ValeriyaM/BTI-Center-for-Expertise-and-Inventory'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+
+activate_this = '/home/ValeriyaM/BTI-Center-for-Expertise-and-Inventory/venv/bin/activate_this.py'
+with open(activate_this) as f:
+    exec(f.read(), dict(__file__=activate_this))
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BTIWebsite.settings')
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
